aa138e373d6bdf7c3b1f18b20b5bf859080fef33
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-04-08 08:41:15 -0300

    ej 1 temrinado

---
+++ Assets/Scenes/Scripts/Ej1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1 : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+
+    public int num1;
+    public int num2;
+    void Start()
+    {
+        if (num1== num2)
+        {
+            Debug.Log("Son inguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
